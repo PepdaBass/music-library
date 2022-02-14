70b977c868fdda466f24006c6aceb7d818465395
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2022-02-14 13:02:21 -0500

    Added Song component and began writing out rough code for it. Tested the GET request using Postman.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import react from 'react';
 import axios from 'axios';
 import './App.css';
 
---
+++ src/Components/Song/Song.jsx
@@ -0,0 +1,13 @@
+
+
+import React, { useState } from 'react';
+
+
+const Song = (props) => {
+
+    const [song, setSong] = useState([]);
+
+    return (  );
+}
+ 
+export default Song;
